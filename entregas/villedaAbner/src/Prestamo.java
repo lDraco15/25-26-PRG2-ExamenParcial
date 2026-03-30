fe0c37b2ac3574c341df81430e9839e712c942ef
lDraco15 <abnercito.villeda@gmail.com>
2026-03-30 18:05:49 +0200

    Feature: Implementacion de atributos y constructor en prestamo

---
+++ entregas/villedaAbner/src/Prestamo.java
@@ -0,0 +1,11 @@
+public class Prestamo {
+    private final Libro libro;
+    private final Usuario usuario;
+    private final Fecha fechaPrestamo;
+
+    private Fecha fechaDevolucion;
+
+    public Prestamo(Libro libro, Usuario usuario, Fecha fechaPrestamo) {
+        
+    }
+}
